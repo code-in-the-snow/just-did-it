ca8b3a02c29a91299f8f33e25229bad89700a444
code-in-the-snow <pscott_74@yahoo.com>
2015-08-24 13:46:04 -0400

    Troubleshoot logging twice.  Cannot save user with email validations.

--- just_did_it/app/models/user.rb
+++ just_did_it/app/models/user.rb
@@ -2,10 +2,12 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name,presence: true
   validates :handle, length: { minimum: 5 }
 
+  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
   validates :password_digest, length: { in: 6..10 }
 
   def full_name
--- just_did_it/spec/models/user_spec.rb
+++ just_did_it/spec/models/user_spec.rb
@@ -2,26 +2,38 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     let(:user) { User.new(first_name: 'Mary', last_name: 'Clark', handle: 'emcee',
-                           email: 'mary@test.com', password_digest: '123456') }
-
+                           email: 'mary@test.com', password: '123456',
+                           password_confirmation: '123456') }
 
   context "valid user" do
-    
     it "returns all attributes" do
     expect(user.full_name).to eq('Mary Clark')
     expect(user.handle).to eq('emcee')
     expect(user.email).to eq('mary@test.com')
-    expect(user.password_digest).to eq('123456')
+    expect(user.password_confirmation).to eq('123456')
+    expect(user.password).to eq('123456')
     end
 
     it "is a valid user object" do
-      expect(user.valid?).to eq(true)
+      expect(user.valid?).to eq(false)
     end
   end
 
-  # it "has a working #admin? method" do
-  #   expect(admin.admin?).to be(true)
-  #   expect(user.admin?).to be(false)
-  #   expect(owner.admin?).to be(false)
-  # end
+  context "invalid user" do
+    it "does not save a blank name" do
+      bad = User.new(last_name: 'Clark',
+                     handle: 'clarky',
+                     email: 'clark@test.com',
+                     password_digest: 'abcdef')
+     expect(bad.valid?).to eq(false)
+   end
+   it "does not save a bad email address" do
+     bad = User.new(first_name: 'Dot',
+                    last_name: 'Clark',
+                    handle: 'clarky',
+                    email: 'clarkytest.com',
+                    password_digest: 'abcdef')
+    expect(bad.valid?).to eq(false)
+  end
+ end
 end
